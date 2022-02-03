b7fd868be992a262a08fcbda2edddce2c75898fd
Miisterone <miisterone27@gmail.com>
2022-02-03 01:38:32 +0100

    Add a project

---
+++ README.md
@@ -0,0 +1,70 @@
+# PHP WordPress
+
+Voici un plugin pour recevoir sur Discord , les nouveaux commentaires mis sur vos articles wordpress.
+
+## Prérequis
+
+- WordPress
+- Discord
+
+
+## Installation
+
+- Télécharger le dossier du plugin
+
+  ![App Screenshot](https://i.postimg.cc/QNTvDKGs/Capture-d-cran-2022-01-27-11-44-12.png)
+
+- Aller sur votre page administrateur Wordpress
+
+  ![App Screenshot](https://i.postimg.cc/PqX4MB55/Capture-d-cran-2022-01-27-11-45-31.png)
+
+- Cliquer sur **Ajouter** puis **Téléverser une application**
+
+  ![App Screenshot](https://i.postimg.cc/PJjNWkd6/Capture-d-cran-2022-01-27-11-46-31.png)
+
+- Glisser/Choisir le .zip 
+
+- Activer le plugin
+
+  ![App Screenshot](https://i.postimg.cc/9f2V2hY3/83-C98z-UNnq.png)
+
+- Une fois activé, vous n'avez plus qu'à rentrer le ChatID de telegram et/ou le Webhook de votre serveur Discord et le tour est joué ! (À noter que tous les settings sont sauvegardés dans la database)
+
+  ![App Screenshot](https://i.postimg.cc/xjXdt9sg/y-He-OEq9-V3-B.png)
+
+
+- Discord :
+
+  ![App Screenshot](https://i.postimg.cc/D0GnFyHT/PUq-G2-Z7in-L.png)
+
+
+### Ou
+
+Aller dans le **dossier racine** de WordPress > **wp-content > plugins**.
+
+Puis mettre le fichier du plugin.
+
+  ![App Screenshot](https://i.postimg.cc/HLq9WNRj/Capture.png)
+
+Se rendre dans le **dashboard** > **Plugin**
+
+  ![App Screenshot](https://i.postimg.cc/kgJ3YcJ9/c-JAQIe75-WN.png)
+
+
+## Création d'un Webhook
+
+Sélectionner le channel où vous voulez créer votre Webhook.Puis Cliquer sur l'engrenage pour modifier le channel
+
+  ![App Screenshot](https://i.ibb.co/88nN10g/webhook.png)
+
+Ensuite aller voir dans les intégrations, puis "Créer un webhook"
+
+  ![App Screenshot](https://i.ibb.co/px3x0ZB/o-Ta-F5h8-Qd-W.png)
+
+Vous pouvez **changer son nom**, **choisir le salon spécifique** pour recevoir les notifications ou bien **récupérer** l'url du webhook
+
+  ![App Screenshot](https://i.ibb.co/FmFRYLp/h-Moc-MDHl-Fb.png)
+
+## Authors
+
+- Joris Pader
---
+++ plugin_wordpress.php
@@ -0,0 +1,159 @@
+<?php
+/*
+Plugin Name: Discord notification
+Description: The plugin allows you to retrieve comments from a post to publish them on a specific Discord channel. The goal is to receive a Discord notification for each comment posted.
+Author: Pader Joris
+Version: 1.0
+*/
+
+//Plugin initialization
+function init_plugin() {
+    add_option( 'webhook' );
+    add_option( 'bot_comment' );
+    add_option( 'bot_name' );
+    add_option( 'bot_message' );
+    add_option( 'bot_author' );
+}
+
+register_activation_hook( __FILE__, 'init_plugin' );
+
+//Send to Discord
+function discord_notif( $comment_ID, $comment_approved ) {
+    if ( 1 === $comment_approved ) {
+        $comment   = get_comment( $comment_ID );
+        $post_id   = $comment->comment_post_ID;
+        $timestamp = date( "c", strtotime( "now" ) );
+
+        $json_data = json_encode( [
+
+            "username" => get_option( 'bot_name' ),
+            "tts"      => false,
+            "content"  => get_option( 'bot_message' ),
+
+            "embeds" => [
+                [
+                    "title" => "Title of post: " . get_the_title( $post_id ),
+
+                    "type" => "rich",
+
+                    "description" => get_option( 'bot_comment' ) . ": " . $comment->comment_content,
+
+                    "timestamp" => $timestamp,
+
+                    "color" => hexdec( "3366ff" ),
+
+                    "author" => [
+                        "name" => get_option( 'bot_author' ) . ": " . $comment->comment_author,
+                    ],
+                ]
+            ]
+        ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+        $ch = curl_init( get_option( 'webhook' ) );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Content-type: application/json' ) );
+        curl_setopt( $ch, CURLOPT_POST, 1 );
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data );
+        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
+        curl_setopt( $ch, CURLOPT_HEADER, 0 );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+
+        $response = curl_exec( $ch );
+        curl_close( $ch );
+    }
+}
+
+add_action( 'comment_post', 'discord_notif', 10, 2 );
+
+//Admin Dashboard Menu
+function notification_admin_menu() {
+    add_menu_page( 'Discord', 'Discord', 'manage_options', 'notifications-admin-menu-discord', 'notifications_admin_menu_discord', 'dashicons-format-chat', 2 );
+}
+
+add_action( 'admin_menu', 'notification_admin_menu' );
+
+//Plugin settings and display in admin dashboard
+function notifications_admin_menu_discord() {
+    ?>
+    <form action="admin.php?page=notifications-admin-menu-discord" method="post">
+        <div class="wrapper" style="display:grid;grid-template-columns: 400px 400px;">
+            <div>
+                <h1>
+                    <?php esc_html_e( 'Discord WebHook', 'notif_discord' ); ?>
+                </h1>
+                <input type="text" name="webhook" minlength="32"
+                       placeholder="<?php if ( get_option( 'webhook' ) != null ) {
+                           echo get_option( 'webhook' );
+                       } else echo "Entre webhook" ?>">
+            </div>
+            <div>
+                <h1>
+                    <?php esc_html_e( 'Bot message', 'bot_message' ); ?>
+                </h1>
+                <input type="text" name="bot_message" minlength="1"
+                       placeholder="<?php if ( get_option( 'bot_message' ) != null ) {
+                           echo get_option( 'bot_message' );
+                       } else echo "Entre message" ?>">
+            </div>
+            <div>
+                <h1>
+                    <?php esc_html_e( 'Message for new comments', 'bot_comment' ); ?>
+                </h1>
+                <input type="text" name="bot_comment" minlength="1"
+                       placeholder="<?php if ( get_option( 'bot_comment' ) != null ) {
+                           echo get_option( 'bot_comment' );
+                       } else echo "Entre text" ?>">
+            </div>
+            <div>
+                <h1>
+                    <?php esc_html_e( 'Author', 'bot_author' ); ?>
+                </h1>
+                <input type="text" name="bot_author" minlength="1"
+                       placeholder="<?php if ( get_option( 'bot_author' ) != null ) {
+                           echo get_option( 'bot_author' );
+                       } else echo "Entre text" ?>">
+            </div>
+            <div>
+                <h1>
+                    <?php esc_html_e( 'Bot name', 'bot_name' ); ?>
+                </h1>
+                <input type="text" name="bot_name" minlength="1"
+                       placeholder="<?php if ( get_option( 'bot_name' ) != null ) {
+                           echo get_option( 'bot_name' );
+                       } else echo "Entre name" ?>">
+            </div>
+        </div>
+        <br>
+        <input type="submit" name="submit" value="Save">
+        <?php
+        $webhookurl  = "";
+        $bot_comment = "";
+        $bot_name    = "";
+        $bot_message = "";
+        $bot_author  = "";
+        if ( isset( $_POST['submit'] ) ) {
+            $webhookurl  = $_POST['webhook'];
+            $bot_comment = $_POST['bot_comment'];
+            $bot_name    = $_POST['bot_name'];
+            $bot_message = $_POST['bot_message'];
+            $bot_author  = $_POST['bot_author'];
+            echo "Settings save";
+        }
+        if ( $webhookurl != "" && $webhookurl != get_option( 'webhook' ) ) {
+            $options = update_option( 'webhook', $webhookurl );
+        }
+        if ( $bot_comment != "" && $bot_comment != get_option( 'bot_comment' ) ) {
+            $options = update_option( 'bot_comment', $bot_comment );
+        }
+        if ( $bot_name != "" && $bot_name != get_option( 'bot_name' ) ) {
+            $options = update_option( 'bot_name', $bot_name );
+        }
+        if ( $bot_message != "" && $bot_message != get_option( 'bot_message' ) ) {
+            $options = update_option( 'bot_message', $bot_message );
+        }
+        if ( $bot_author != "" && $bot_author != get_option( 'bot_author' ) ) {
+            $options = update_option( 'bot_author', $bot_author );
+        }
+        ?>
+    </form>
+    <?php
+}
